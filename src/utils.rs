a56690a060f68cc5b162942db48fe0a3682ddd0a
Zelimir Fedoran <zelimir.fedoran@gmail.com>
2025-04-21 14:30:44 -0400

    use syscall for hash

--- src/utils.rs
+++ src/utils.rs
@@ -1,21 +1,15 @@
-use sha2::{Digest, Sha256};
 use super::hash::Hash;
 use super::error::{ProgramError, ProgramResult};
 
 #[inline]
 pub fn hash(data: &[u8]) -> Hash {
-    let mut hasher = Sha256::new();
-    hasher.update(data);
-    Hash::new_from_array(hasher.finalize().into())
+    Hash::new_from_array(hashv(&[data]).into())
 }
 
 #[inline]
 pub fn hashv(data: &[&[u8]]) -> Hash {
-    let mut hasher = Sha256::new();
-    for d in data {
-        hasher.update(d);
-    }
-    Hash::new_from_array(hasher.finalize().into())
+    let res = solana_program::hash::hashv(data);
+    Hash::new_from_array(res.to_bytes())
 }
 
 #[inline]
